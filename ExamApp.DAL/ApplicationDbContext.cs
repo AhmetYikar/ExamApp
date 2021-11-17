fe58d7d4181a1d29976ce1d1217ed32232ea128b
ahmet yıkar <ahmetyikar@hotmail.com.tr>
2021-11-17 22:11:34 +0300

    Add project files.

---
+++ ExamApp.DAL/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ExamApp.Entity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ExamApp.DAL
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Question> Questions { get; set; }
+    }
+}
---
+++ ExamApp.Entity/Question.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExamApp.Entity
+{
+    public enum Answer : byte
+    {
+        A,
+        B,
+        C,
+        D
+    }
+    public class Question : IValidatableObject
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? ModifiedAt { get; set; }
+
+        [Required(ErrorMessage = "Soru boş geçilemez.")]
+        [MaxLength(512, ErrorMessage = "Soru 512 karakterden uzun olamaz.")]
+        public string QuestionTitle { get; set; }
+
+        [MaxLength(4096, ErrorMessage = "Soru metni 4096 karakterden uzun olamaz.")]
+        public string QuestionText { get; set; }
+
+
+        [Required(ErrorMessage = "Şıklar boş geçilemez.")]
+        [MaxLength(512, ErrorMessage = "Şıklar 512 karakterden uzun olamaz.")]
+        public string A { get; set; }
+
+        [Required(ErrorMessage = "Şıklar boş geçilemez.")]
+        [MaxLength(512, ErrorMessage = "Şıklar 512 karakterden uzun olamaz.")]
+        public string B { get; set; }
+
+        [Required(ErrorMessage = "Şıklar boş geçilemez.")]
+        [MaxLength(512, ErrorMessage = "Şıklar 512 karakterden uzun olamaz.")]
+        public string C { get; set; }
+
+        [Required(ErrorMessage = "Şıklar boş geçilemez.")]
+        [MaxLength(512, ErrorMessage = "Şıklar 512 karakterden uzun olamaz.")]
+        public string D { get; set; }
+
+        public Answer Answer { get; set; }
+
+        //for custom model validation 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
